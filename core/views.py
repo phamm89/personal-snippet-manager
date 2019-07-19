a261a335bccb797d1a49c7ceb04e434923531a2b
Michael <phamm89@gmail.com>
2019-07-18 21:52:14 -0400

    Able to add snippets

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, redirect
 from core.models import Snippet
+from core.forms import SnippetForm
+from django.views.generic.edit import CreateView
 
 # Views created for Code Snippet
 def index(request):
@@ -14,3 +16,17 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+# Method for adding a snippet
+def add_snippet(request):
+    snippet = get_list_or_404(Snippet)
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            snippet = form.save(commit=False)
+            snippet.post = Snippet
+            form.save(Snippet)
+            return redirect('index')
+    else:
+        form = SnippetForm()
+    return render(request, 'core/snippet_form.html', {'form':form})
