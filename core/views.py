10fedc3282b1c3a495dfca72842392918859e340
Michael <phamm89@gmail.com>
2019-07-18 21:00:39 -0400

    Working on Displaying Snippets

--- core/views.py
+++ core/views.py
@@ -5,9 +5,12 @@ from core.models import Snippet
 def index(request):
     """View function for home page of site."""
 
-    snippet_codes = Snippet.objects.all().count()
+    snippet_codes_count = Snippet.objects.all().count()
+    snippet_codes = Snippet.objects.all()
+
     context = {
-        'snippet_codes': snippet_codes,
+        'snippet_codes_count': snippet_codes_count,
+        'snippet_codes': snippet_codes
     }
 
     return render(request, 'index.html', context=context)
